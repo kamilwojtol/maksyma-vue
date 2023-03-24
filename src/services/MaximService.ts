43e0526795b7ff915b7d91f49f5525facddc311c
Kamil Wojtol <kamil.wojtol97@gmail.com>
2023-03-24 10:36:33 +0100

    added try catch for service

--- src/services/MaximService.ts
+++ src/services/MaximService.ts
@@ -1,17 +1,27 @@
+import type { Quote } from "@/interfaces/IQuote";
+
 const URL = "https://api.quotable.io";
 
 const getRandomQuote = async () => {
-  const request = await fetch(`${URL}/random`);
-  const data = await request.json();
+  try {
+    const request = await fetch(`${URL}/random`);
+    const data = await request.json();
 
-  return data;
+    return data;
+  } catch {
+    throw new Error("Problem with fetching random quote data");
+  }
 };
 
 const getQuotesListWithTag = async (tag: string) => {
-  const request = await fetch(`${URL}/quotes?tags=${tag}`);
-  const data = await request.json();
+  try {
+    const request = await fetch(`${URL}/quotes?tags=${tag}`);
+    const data = await request.json();
 
-  return data;
+    return data;
+  } catch {
+    throw new Error("Problem with fetching list data");
+  }
 };
 
 export { getRandomQuote, getQuotesListWithTag };
